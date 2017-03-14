6376691fa1afcc48d4869134949d5766a63ae9aa
forsooth <maastntes@gmail.com>
2017-03-13 23:32:47 -0400

    Editing database start script so that migrations do not give an error.

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 d=$(dirname $0)
-$d/utils/stop_db.sh
+$d/utils/stop_db.sh 2> /dev/null
 pids=$(ps auxww | grep -E "angular|runserve" | grep -v grep | awk '{print $2}')
 for pid in $pids; do
 	echo "Killing PID $pid"
--- utils/start_db.sh
+++ utils/start_db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd /
 sudo -u postgres /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data/ -l /usr/local/pgsql/logs/logfile start
 
