c964f98a8e708e754acdffad64c340da22fb58e8
forsooth <maastntes@gmail.com>
2017-04-06 05:02:23 -0400

    Running deploy commands as ec2-user account.

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ cd /home/ec2-user/cardcontrol
 
 sleep 5
 
-es=$(git status -uno | wc -l)
+es=$(sudo -u ec2-user git status -uno | wc -l)
 
 if [[ "$es" == "2" ]]; then
         echo "[$(date +%Y-%m-%d:%H:%M:%S)]: No updates found." >> /home/ec2-user/cardcontrol/deploy.log
         exit 1
 fi
 
-bash /home/ec2-user/cardcontrol/stop.sh
-git stash
-git checkout deploy && git pull origin deploy
-bash /home/ec2-user/cardcontrol/start.sh
+sudo -u ec2-user bash /home/ec2-user/cardcontrol/stop.sh 2>> /home/ec2-user/cardcontrol/deploy.log
+sudo -u ec2-user git stash 2>> /home/ec2-user/cardcontrol/deploy.log
+sudo -u ec2-user git checkout deploy && git pull origin deploy 2>> /home/ec2-user/cardcontrol/deploy.log
+sudo -u ec2-user bash /home/ec2-user/cardcontrol/start.sh 2>> /home/ec2-user/cardcontrol/deploy.log
 
 echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Attempted to pull." >> /home/ec2-user/cardcontrol/deploy.log
 
