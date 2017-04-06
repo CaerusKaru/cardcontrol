0f6cb9e9af917735a095e51631694b08fd6dd9e3
forsooth <maastntes@gmail.com>
2017-04-06 04:50:56 -0400

    Breaking up commands in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,17 @@ cd /home/ec2-user/cardcontrol
 sleep 5
 
 es=$(git status -uno | wc -l)
+
 if [[ "$es" == "2" ]]; then
         echo "[$(date +%Y-%m-%d:%H:%M:%S)]: No updates found." >> /home/ec2-user/cardcontrol/deploy.log
         exit 1
 fi
 
-/home/ec2-user/cardcontrol/stop.sh && git stash && git checkout deploy && git pull origin deploy && /home/ec2-user/cardcontrol/start.sh
+bash /home/ec2-user/cardcontrol/stop.sh
+git stash
+git checkout deploy && git pull origin deploy
+bash /home/ec2-user/cardcontrol/start.sh
+
 echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Attempted to pull." >> /home/ec2-user/cardcontrol/deploy.log
 
 
