f5e237dfc1506a6280c06db2d19c7c55aa8cb245
forsooth <maastntes@gmail.com>
2017-04-06 04:45:31 -0400

    Tweaking deploy script to cd to application directory.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Starting deployment." >> /home/ec2-user/cardcontrol/deploy.log
 
+cd /home/ec2-user/cardcontrol
+
 sleep 5
 
 es=$(git status -uno | wc -l)
@@ -8,6 +10,7 @@ if [[ "$es" == "2" ]]; then
         echo "[$(date +%Y-%m-%d:%H:%M:%S)]: No updates found." >> /home/ec2-user/cardcontrol/deploy.log
         exit 1
 fi
+
 /home/ec2-user/cardcontrol/stop.sh && git stash && git checkout deploy && git pull origin deploy && /home/ec2-user/cardcontrol/start.sh
 echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Attempted to pull." >> /home/ec2-user/cardcontrol/deploy.log
 
