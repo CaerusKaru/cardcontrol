b1edaddd1d55f5661463949f83ebd456ff77dc59
forsooth <maastntes@gmail.com>
2017-04-06 01:43:22 -0400

    Add update needed check to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sleep 5
+es=$(git status -uno -C ~/cardcontrol/ | wc -l)
+if [[ "$es" == "2" ]]; then
+	exit 0;
+fi;
 ~/cardcontrol/stop.sh && git -C ~/cardcontrol/ stash && git -C ~/cardcontrol/ checkout deploy && git -C ~/cardcontrol/ pull origin deploy && ~/cardcontrol/start.sh
 
