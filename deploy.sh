c65df03b9c6b7571a9f69ca27364775e5e763150
forsooth <maastntes@gmail.com>
2017-04-06 02:16:21 -0400

    Change working branch of git webhook.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 sleep 5
 es=$(git status -uno -C ~/cardcontrol/ | wc -l)
 if [[ "$es" == "2" ]]; then
-	exit 0;
-fi;
+	echo "[$(date +%Y-%m-%d:%H:%M:%S)]: No updates found." > ~/cardcontrol/deploy.log
+      	exit 1
+fi
 ~/cardcontrol/stop.sh && git -C ~/cardcontrol/ stash && git -C ~/cardcontrol/ checkout deploy && git -C ~/cardcontrol/ pull origin deploy && ~/cardcontrol/start.sh
+echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Attempted to pull." > ~/cardcontrol/deploy.log
 
