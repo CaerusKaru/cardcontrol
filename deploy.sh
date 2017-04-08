a3476632c449c37c4fd7b0ec9140144a2325cd81
forsooth <maastntes@gmail.com>
2017-04-08 06:28:14 -0400

    Getting deploy script working with github webhook.

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Starting deployment." >> /home/ec2-user/cardcontrol/deploy.log
+log='/home/ec2-user/cardcontrol/log/deploysh.log'
+
+if [[ ! -w $log ]]; then echo "" > $log; fi
+
+echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Starting deployment." >> $log 
 
 cd /home/ec2-user/cardcontrol
 
-sleep 5
+es=$(git remote show origin | grep 'pushes to dev' | grep 'local out of date' | wc -l)
 
-es=$(sudo -u ec2-user git status -uno | wc -l)
-
-if [[ "$es" == "2" ]]; then
-        echo "[$(date +%Y-%m-%d:%H:%M:%S)]: No updates found." >> /home/ec2-user/cardcontrol/deploy.log
-        exit 1
+if [[ "$es" -eq "0" ]]; then
+        echo "[$(date +%Y-%m-%d:%H:%M:%S)]: No updates found." >> $log
+        exit 103
 fi
 
-sudo -u ec2-user bash /home/ec2-user/cardcontrol/stop.sh 2>> /home/ec2-user/cardcontrol/deploy.log
-sudo -u ec2-user git stash 2>> /home/ec2-user/cardcontrol/deploy.log
-sudo -u ec2-user git checkout deploy && git pull origin deploy 2>> /home/ec2-user/cardcontrol/deploy.log
-sudo -u ec2-user bash /home/ec2-user/cardcontrol/start.sh 2>> /home/ec2-user/cardcontrol/deploy.log
+bash /home/ec2-user/cardcontrol/stop.sh 2>> /home/ec2-user/cardcontrol/log/deploysh.log
+git stash 2>> /home/ec2-user/cardcontrol/log/deploysh.log
+git checkout deploy && git pull origin dev 2>> /home/ec2-user/cardcontrol/log/deploysh.log
+bash /home/ec2-user/cardcontrol/start.sh 2>> /home/ec2-user/cardcontrol/log/deploysh.log
 
-echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Attempted to pull." >> /home/ec2-user/cardcontrol/deploy.log
-
+echo "[$(date +%Y-%m-%d:%H:%M:%S)]: Attempted to pull." >> $log
 
