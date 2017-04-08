2666dc69f7501d692a44395ba8c7b65a89d164c7
forsooth <maastntes@gmail.com>
2017-04-08 18:57:13 -0400

    Editing ng build script to output to log file.

--- ng-build.sh
+++ ng-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+d=$(dirname "$0")
+
 set -e
 sudo npm install
-node --max_old_space_size=900 ./node_modules/.bin/ng build --prod --verbose --output-path /home/ec2-user/cardcontrol/dist/
+node --max_old_space_size=900 ./node_modules/.bin/ng build --prod --verbose --output-path /home/ec2-user/cardcontrol/dist/ 2>$d/log/ngbuild.log
 
 sudo rm -r /dist/
 sudo mv /home/ec2-user/cardcontrol/dist/ /dist
