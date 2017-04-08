adc815d74519303cc339faac9058ae4caf2086db
forsooth <maastntes@gmail.com>
2017-04-08 17:42:34 -0400

    Fixing merge conflict in ng build script/.

--- ng-build.sh
+++ ng-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sudo npm install
-node --max_old_space_size=2000 ./node_modules/.bin/ng build --prod --verbose --output-path /home/ec2-user/cardcontrol/dist/
+node --max_old_space_size=900 ./node_modules/.bin/ng build --prod --verbose --output-path /home/ec2-user/cardcontrol/dist/
 
 sudo rm -r /dist/
 sudo mv /home/ec2-user/cardcontrol/dist/ /dist
