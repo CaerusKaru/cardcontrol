fb64a59bcba27364bfcedb17b785caff6894c966
forsooth <maastntes@gmail.com>
2017-04-08 17:42:08 -0400

    Editing ng-build script

--- ng-build.sh
+++ ng-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+sudo npm install
 node --max_old_space_size=2000 ./node_modules/.bin/ng build --prod --verbose --output-path /home/ec2-user/cardcontrol/dist/
 
 sudo rm -r /dist/
