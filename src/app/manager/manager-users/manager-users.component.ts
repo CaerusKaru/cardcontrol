afc7f9ccce03421c1a8b1e10b686f56bee1a8c43
Adam Plumer <caerus.karu@gmail.com>
2017-04-25 09:05:53 -0400

    fixes manager users view bug which cleared the list of access points on change for a manager

--- src/app/manager/manager-users/manager-users.component.ts
+++ src/app/manager/manager-users/manager-users.component.ts
@@ -107,9 +107,10 @@ export class ManagerUsersComponent implements OnInit {
     dialogRef.componentInstance.userAccount = this.userAccount;
     dialogRef.afterClosed().subscribe(data => {
       if (data) {
-        data.access_points = data.access_points.map(a => a.resource_uri);
-        data.access_points_managed = data.access_points_managed.map(a => a.resource_uri);
-        this.requestService.updateUserAccount(data);
+        let newData = Object.assign({}, data);
+        newData.access_points = newData.access_points.map(a => a.resource_uri);
+        newData.access_points_managed = newData.access_points_managed.map(a => a.resource_uri);
+        this.requestService.updateUserAccount(newData);
       }
     });
   }
